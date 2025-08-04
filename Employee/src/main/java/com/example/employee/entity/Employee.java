f93cb9d0a7c777211a8674ee1829aa2363a4ead2
GayatriDhanave <gayatridhanave7@gmail.com>
2025-08-04 15:28:39 +0530

    Data types demonstration

---
+++ Employee/src/main/java/com/example/employee/entity/Employee.java
@@ -0,0 +1,23 @@
+package com.example.employee.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int eid;
+    String name;
+    short age;
+    char gender;
+    String address;
+    long salary;
+    float bonus;
+    byte availableLeaves;
+    
+
+
+}
